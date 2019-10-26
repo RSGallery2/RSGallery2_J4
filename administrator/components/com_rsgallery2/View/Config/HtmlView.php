432a6485091d2031ae1117469d64a25569ca2c48
ThomasFinnernVM <github@tomfinnern.de>
2019-10-26 17:40:56 +0200

    Layout test for config raw view

--- administrator/components/com_rsgallery2/View/Config/HtmlView.php
+++ administrator/components/com_rsgallery2/View/Config/HtmlView.php
@@ -12,11 +12,14 @@ namespace Joomla\Component\Rsgallery2\Administrator\View\Config;
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Router\Route;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\Toolbar\Toolbar;
 use Joomla\CMS\Toolbar\ToolbarHelper;
+
+use Joomla\Component\Rsgallery2\Administrator\Helper\Rsgallery2Helper;
 
 /**
  * View class for a list of rsgallery2.
@@ -39,10 +42,58 @@ class HtmlView extends BaseHtmlView
 	public function display($tpl = null)
 	{
 
+		$Layout = Factory::getApplication()->input->get('layout');
+		echo '$Layout: ' . $Layout . '<br>' ;
+		//echo '$tpl: ' . json_encode($tpl) . '<br>' ;
+		echo '$tpl: ' . $tpl . '<br>' ;
 
-		echo 'HtmlView.php: ' . realpath(dirname(__FILE__)) . '<br>';
+
+
+		echo 'HtmlView.php: ' . realpath(dirname(__FILE__)) . '<br>' ;
+
+		//---  --------------------------------------------------------------
+
+		HTMLHelper::_('sidebar.setAction', 'index.php?option=com_rsgallery2&view=config&layout=RawView');
+		/**/
+		$Layout = Factory::getApplication()->input->get('layout');
+		Rsgallery2Helper::addSubmenu('maintenance');
+		$this->sidebar = \JHtmlSidebar::render();
+
+		$this->addToolbar($Layout);
+		/**/
 
 		return parent::display($tpl);
+	}
+
+	/**
+	 * Add the page title and toolbar.
+	 *
+	 * @return  void
+	 *
+	 * @since   1.0
+	 */
+	protected function addToolbar($Layout)
+	{
+		// Get the toolbar object instance
+		$toolbar = Toolbar::getInstance('toolbar');
+
+		// on develop show open tasks if existing
+		if (!empty ($this->isDevelop))
+		{
+			echo '<span style="color:red">'
+//				. '*  <br>'
+//				. '*  <br>'
+//				. '*  <br>'
+//				. '*  <br>'
+//				. '*  <br>'
+//				. '*  <br>'
+				. '</span><br><br>';
+		}
+
+		// Set the title
+		ToolBarHelper::title(Text::_('YYYYY'), 'screwdriver'); // 'maintenance');
+
+		$toolbar->preferences('com_rsgallery2');
 	}
 
 
