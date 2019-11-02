4d3d5dc907fe4717b62787abc11c0b510faa0fe8
Thomas.Finnern <github@tomfinnern.de>
2019-11-02 09:40:40 +0100

    Restart empty with gallery table

--- administrator/components/com_rsgallery2/sql/install.mysql.utf8.sql
+++ administrator/components/com_rsgallery2/sql/install.mysql.utf8.sql
@@ -3,136 +3,63 @@
 --
 
 CREATE TABLE IF NOT EXISTS `#__rsg2_galleries` (
-  "id" serial NOT NULL,
-  "asset_id" bigint DEFAULT 0 NOT NULL
-  "parent_id" integer DEFAULT 0 NOT NULL,
-  "lft" bigint DEFAULT 0 NOT NULL,
-  "rgt" bigint DEFAULT 0 NOT NULL,
-  "level" integer DEFAULT 0 NOT NULL,
-  "path" varchar(255) DEFAULT '' NOT NULL,
-  `name` varchar(255) DEFAULT '' NOT NULL,
-  "alias" varchar(255) DEFAULT '' NOT NULL,
-  "note" varchar(255) DEFAULT '' NOT NULL,
-  "description" text DEFAULT '' NOT NULL,
-  "published" smallint DEFAULT 0 NOT NULL,
-  "checked_out" bigint DEFAULT 0 NOT NULL,
-  "checked_out_time" timestamp without time zone DEFAULT '1970-01-01 00:00:00' NOT NULL,
-  "access" bigint DEFAULT 0 NOT NULL,
-  "params" text DEFAULT '' NOT NULL,
-  "metadesc" varchar(1024) DEFAULT '' NOT NULL,
-  "metakey" varchar(1024) DEFAULT '' NOT NULL,
-  "metadata" varchar(2048) DEFAULT '' NOT NULL,
-  "created_user_id" integer DEFAULT 0 NOT NULL,
-  "created_time" timestamp without time zone DEFAULT '1970-01-01 00:00:00' NOT NULL,
-  "modified_user_id" integer DEFAULT 0 NOT NULL,
-  "modified_time" timestamp without time zone DEFAULT '1970-01-01 00:00:00' NOT NULL,
-  "hits" integer DEFAULT 0 NOT NULL,
-  "language" varchar(7) DEFAULT '' NOT NULL,
-  "version" bigint DEFAULT 1 NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
 
-  `thumb_id` int(11) unsigned NOT NULL default '0',
+  `description` text DEFAULT '' NOT NULL,
 
-  PRIMARY KEY ("id")
-);
+  `note` varchar(255) NOT NULL DEFAULT '',
+  `published` tinyint(1) NOT NULL DEFAULT 0, 
+  `checked_out` int(10) unsigned NOT NULL DEFAULT 0,
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00', 
+  `params` text DEFAULT '' NOT NULL,
 
---
--- images
---
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_by` int(10) unsigned NOT NULL DEFAULT 0,
+  `created_by_alias` varchar(255) NOT NULL DEFAULT '',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` int(10) unsigned NOT NULL DEFAULT 0, 
 
-CREATE TABLE IF NOT EXISTS `#__rsgallery2_imagess` (
-  "id" serial NOT NULL,
-  `name` varchar(255) NOT NULL default '',
-  `alias` varchar(255) NOT NULL DEFAULT '',
-  `descr` text,
-  `gallery_id` int(9) unsigned NOT NULL default '0',
-  `title` varchar(255) NOT NULL default '',
-  `hits` int(11) unsigned NOT NULL default '0',
-  `date` datetime NOT NULL default '0000-00-00 00:00:00',
-  `rating` int(10) unsigned NOT NULL default '0',
-  `votes` int(10) unsigned NOT NULL default '0',
-  `comments` int(10) unsigned NOT NULL default '0',
-  `published` tinyint(1) NOT NULL default '1',
-  `checked_out` int(11) NOT NULL default '0',
-  `checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
-  `ordering` int(9) unsigned NOT NULL default '0',
-  `approved` tinyint(1) unsigned NOT NULL default '1',
-  `userid` int(10) NOT NULL,
-  `params` text NOT NULL,
-  `asset_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'FK to the #__assets table.',
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `UK_name` (`name`),
-  KEY `id` (`id`)
-)ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `access` int(11) DEFAULT 0 NOT NULL,
+  
+  `hits` int(10) unsigned NOT NULL DEFAULT 0, 
+  
+--  `rtl` tinyint(4) NOT NULL DEFAULT 0,  
+--  `language` char(7) NOT NULL DEFAULT '', 
 
+--  `metakey` text NOT NULL,
+--  `metadesc` text NOT NULL,
+--  `metadata` text NOT NULL, 
+
+--  `publish_up` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+--  `publish_down` datetime NOT NULL DEFAULT '0000-00-00 00:00:00', 
+
+  `asset_id` int(11)  NOT NULL DEFAULT 0,
+  `parent_id` int(11)  NOT NULL DEFAULT 0,
+  `lft` int(11) DEFAULT 0 NOT NULL,
+  `rgt` int(11) DEFAULT 0 NOT NULL,
+  `level` integer DEFAULT 0 NOT NULL,
+  `path` varchar(400) NOT NULL DEFAULT '',
+
+  
+  PRIMARY KEY (`id`),
+  KEY `idx_access` (`access`),
+  KEY `idx_checkout` (`checked_out`),
+  KEY `idx_state` (`published`),
+--  KEY `idx_catid` (`catid`),
+--  KEY `idx_language` (`language`),
+  KEY `idx_createdby` (`created_by`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
 --
--- Image resolutions
---
+--  
 
-
-
-
----
---- comments
----
-
-CREATE TABLE IF NOT EXISTS `#__rsgallery2_comments` (
-  "id" serial NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `user_name` varchar(100) NOT NULL,
-  `user_ip` varchar(50) NOT NULL default '0.0.0.0',
-  `parent_id` int(11) NOT NULL default '0',
-  `item_id` int(11) NOT NULL,
-  `item_table` varchar(50) default NULL,
-  `datetime` datetime NOT NULL,
-  `subject` varchar(100) default NULL,
-  `comment` text NOT NULL,
-  `published` tinyint(1) NOT NULL default '1',
---- ToDo: `checked_out` int(11) default NULL,
-  `checked_out` int(11) NOT NULL default '0',
---- ToDo: `checked_out_time` datetime default NULL,
-  `checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
-  `ordering` int(11) NOT NULL,
-  `params` text,
-  `hits` int(11) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
----
---- configuration
----
-
-CREATE TABLE IF NOT EXISTS `#__rsgallery2_config` (
-  "id" serial NOT NULL,
-  `name` text NOT NULL,
-  `value` text NOT NULL,
- PRIMARY KEY `id` (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-
----
---- acl permissions
----
-
-CREATE TABLE IF NOT EXISTS `#__rsgallery2_acl` (
-  "id" serial NOT NULL,
-  `gallery_id` int(11) NOT NULL,
-  `parent_id` int(11) NOT NULL default '0',
-  `public_view` tinyint(1) NOT NULL default '1',
-  `public_up_mod_img` tinyint(1) NOT NULL default '0',
-  `public_del_img` tinyint(1) NOT NULL default '0',
-  `public_create_mod_gal` tinyint(1) NOT NULL default '0',
-  `public_del_gal` tinyint(1) NOT NULL default '0',
-  `public_vote_view` tinyint( 1 ) NOT NULL default '1',
-  `public_vote_vote` tinyint( 1 ) NOT NULL default '0',
-  `registered_view` tinyint(1) NOT NULL default '1',
-  `registered_up_mod_img` tinyint(1) NOT NULL default '1',
-  `registered_del_img` tinyint(1) NOT NULL default '0',
-  `registered_create_mod_gal` tinyint(1) NOT NULL default '1',
-  `registered_del_gal` tinyint(1) NOT NULL default '0',
-  `registered_vote_view` tinyint( 1 ) NOT NULL default '1',
-  `registered_vote_vote` tinyint( 1 ) NOT NULL default '1',
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
-
+--  `created_user_id` integer DEFAULT 0 NOT NULL,
+--  `created_time` timestamp without time zone DEFAULT '1970-01-01 00:00:00' NOT NULL,
+--  `modified_user_id` integer DEFAULT 0 NOT NULL,
+--  `modified_time` timestamp without time zone DEFAULT '1970-01-01 00:00:00' NOT NULL,
+--  `hits` integer DEFAULT 0 NOT NULL,
+--  `language` varchar(7) DEFAULT '' NOT NULL,
+--  `version` int(11) DEFAULT 1 NOT NULL,
+--  `thumb_id` int(11) unsigned NOT NULL default '0',
