638de91c139ba403fec3f6e908be07fd4d9910db
Thomas.Finnern <github@tomfinnern.de>
2019-11-02 14:40:41 +0100

    Aligned items to match category table in most parts

--- administrator/components/com_rsgallery2/sql/install.mysql.utf8.sql
+++ administrator/components/com_rsgallery2/sql/install.mysql.utf8.sql
@@ -6,24 +6,30 @@ CREATE TABLE IF NOT EXISTS `#__rsg2_galleries` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT '',
   `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
-
   `description` text DEFAULT '' NOT NULL,
+  `note` varchar(255) NOT NULL DEFAULT '',  
+  `params` text DEFAULT '' NOT NULL,
+  `thumb_id` int(11) unsigned NOT NULL default '0',
 
-  `note` varchar(255) NOT NULL DEFAULT '',
   `published` tinyint(1) NOT NULL DEFAULT 0, 
+  `hits` int(10) unsigned NOT NULL DEFAULT 0, 
+  
   `checked_out` int(10) unsigned NOT NULL DEFAULT 0,
   `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00', 
-  `params` text DEFAULT '' NOT NULL,
-
   `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `created_by` int(10) unsigned NOT NULL DEFAULT 0,
   `created_by_alias` varchar(255) NOT NULL DEFAULT '',
   `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified_by` int(10) unsigned NOT NULL DEFAULT 0, 
 
-  `access` int(11) DEFAULT 0 NOT NULL,
-  
-  `hits` int(10) unsigned NOT NULL DEFAULT 0, 
+  `lft` int(11) DEFAULT 0 NOT NULL,
+  `rgt` int(11) DEFAULT 0 NOT NULL,
+  `level` integer DEFAULT 0 NOT NULL,
+  `path` varchar(400) NOT NULL DEFAULT '',
+   -- ToDO: Does parent Id interfere with nested tree ? May need access and write function to the data itself
+  `parent_id` int(11)  NOT NULL DEFAULT 0,
+
+  `asset_id` int(11)  NOT NULL DEFAULT 0,
   
 --  `rtl` tinyint(4) NOT NULL DEFAULT 0,  
 --  `language` char(7) NOT NULL DEFAULT '', 
@@ -34,19 +40,13 @@ CREATE TABLE IF NOT EXISTS `#__rsg2_galleries` (
 
 --  `publish_up` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
 --  `publish_down` datetime NOT NULL DEFAULT '0000-00-00 00:00:00', 
-
-  `asset_id` int(11)  NOT NULL DEFAULT 0,
-  `parent_id` int(11)  NOT NULL DEFAULT 0,
-  `lft` int(11) DEFAULT 0 NOT NULL,
-  `rgt` int(11) DEFAULT 0 NOT NULL,
-  `level` integer DEFAULT 0 NOT NULL,
-  `path` varchar(400) NOT NULL DEFAULT '',
-
+  `access` int(10) unsigned NOT NULL DEFAULT 0, 
   
   PRIMARY KEY (`id`),
-  KEY `idx_access` (`access`),
+--  KEY `idx_access` (`access`),
   KEY `idx_checkout` (`checked_out`),
   KEY `idx_state` (`published`),
+	KEY `idx_left_right` (`lft`, `rgt`), 
 --  KEY `idx_catid` (`catid`),
 --  KEY `idx_language` (`language`),
   KEY `idx_createdby` (`created_by`)
@@ -55,11 +55,3 @@ CREATE TABLE IF NOT EXISTS `#__rsg2_galleries` (
 --
 --  
 
---  `created_user_id` integer DEFAULT 0 NOT NULL,
---  `created_time` timestamp without time zone DEFAULT '1970-01-01 00:00:00' NOT NULL,
---  `modified_user_id` integer DEFAULT 0 NOT NULL,
---  `modified_time` timestamp without time zone DEFAULT '1970-01-01 00:00:00' NOT NULL,
---  `hits` integer DEFAULT 0 NOT NULL,
---  `language` varchar(7) DEFAULT '' NOT NULL,
---  `version` int(11) DEFAULT 1 NOT NULL,
---  `thumb_id` int(11) unsigned NOT NULL default '0',
