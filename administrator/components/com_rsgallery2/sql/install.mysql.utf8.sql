e40ffbd2624dc667282e1ed0547d8ec2971dcec7
ThomasFinnernVM <github@tomfinnern.de>
2019-11-04 12:55:59 +0100

    Started to improve sql for images / media queries

--- administrator/components/com_rsgallery2/sql/install.mysql.utf8.sql
+++ administrator/components/com_rsgallery2/sql/install.mysql.utf8.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS `#__rsg2_galleries` (
   `alias` varchar(400) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '',
   `description` text DEFAULT '' NOT NULL,
   `note` varchar(255) NOT NULL DEFAULT '',  
-  `params` text DEFAULT '' NOT NULL,
   `thumb_id` int(11) unsigned NOT NULL default '0',
+  `params` text DEFAULT '' NOT NULL,
 
   `published` tinyint(1) NOT NULL DEFAULT 0, 
   `hits` int(10) unsigned NOT NULL DEFAULT 0, 
@@ -26,9 +26,9 @@ CREATE TABLE IF NOT EXISTS `#__rsg2_galleries` (
   `rgt` int(11) DEFAULT 0 NOT NULL,
   `level` integer DEFAULT 0 NOT NULL,
   `path` varchar(400) NOT NULL DEFAULT '',
-  `parent_id` int(11)  NOT NULL DEFAULT 0,
-  `access` int(10) NOT NULL DEFAULT 0, 
   
+  `parent_id` int(11)  NOT NULL DEFAULT 0,
+  `access` int(10) NOT NULL DEFAULT 0,   
   `asset_id` int(11)  NOT NULL DEFAULT 0,
   
 --  `rtl` tinyint(4) NOT NULL DEFAULT 0,  
@@ -53,5 +53,73 @@ CREATE TABLE IF NOT EXISTS `#__rsg2_galleries` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
 
 --
---  
+-- images / files
+--
+
+CREATE TABLE IF NOT EXISTS `#__rsg2_images` (
+  `id` serial NOT NULL,
+  `name` varchar(255) NOT NULL default '',
+  `alias` varchar(255) NOT NULL DEFAULT '',
+  `descr` text,
+  
+  `gallery_id` int(9) unsigned NOT NULL default '0',
+  `title` varchar(255) NOT NULL default '',
+  
+  `date` datetime NOT NULL default '0000-00-00 00:00:00',
+  `rating` int(10) unsigned NOT NULL default '0',
+  `votes` int(10) unsigned NOT NULL default '0',
+  `comments` int(10) unsigned NOT NULL default '0',
+  
+  `published` tinyint(1) NOT NULL default '1',
+  `hits` int(11) unsigned NOT NULL default '0',
+  
+  `checked_out` int(10) unsigned NOT NULL DEFAULT 0,
+  `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00', 
+  `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `created_by` int(10) unsigned NOT NULL DEFAULT 0,
+  `created_by_alias` varchar(255) NOT NULL DEFAULT '',
+  `modified` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `modified_by` int(10) unsigned NOT NULL DEFAULT 0, 
+
+  `ordering` int(9) unsigned NOT NULL default '0',
+  `approved` tinyint(1) unsigned NOT NULL default '1',
+
+  `params` text NOT NULL,
+  `access` int(10) NOT NULL DEFAULT 0,   
+  `asset_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'FK to the #__assets table.',
+  
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `UK_name` (`name`),
+  KEY `id` (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+
+
+--
+-- Media query / Image resolutions
+--
+
+CREATE TABLE IF NOT EXISTS `#__rsg2_media_break` (
+  `id` serial NOT NULL,
+
+  `image_id` int(9) unsigned NOT NULL default '0',
+  'breakpoint' double
+
+#http://www.w3.org/TR/css3-mediaqueries/
+
+#Media type ‘aural’, ‘braille’, ‘handheld’, ‘print’, ‘projection’, ‘screen’, ‘tty’, ‘tv’
+
+# breakpopints: width, 
+#	Viewport-Breite (z.B.: Der zur Verfügung stehende Platz innerhalb des Browserfensters)
+# Beispiel: @media handheld and (min-width: 20em) { ... }heightViewport-Höhe (z.B.: Der zur Verfügung stehende Platz innerhalb des Browserfensters)
+# Beispiel: @media screen and (max-height: 700px) { … } device-widthBreite des Mediums (Smartphone-Bildschirm, Monitorgröße etc. )
+# Beispiel: @media screen and (device-width: 800px) { … }device-heightHöhe des Mediums (Smartphone-Bildschirm, Monitorgröße etc.)
+# Beispiel: @media screen and (device-height: 400px) { … }orientationBeschreibt ob ein Gerät im Querformat (landscape) oder im Hochformat gehalten wird (portrait).
+
+
+
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `UK_name` (`name`),
+  KEY `id` (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
+
 
--- administrator/components/com_rsgallery2/sql/uninstall.mysql.utf8.sql
+++ administrator/components/com_rsgallery2/sql/uninstall.mysql.utf8.sql
@@ -2,12 +2,18 @@
 # uninstall sql for RSGallery2
 #
 
+#DROP TABLE IF EXISTS `#__rsg2_galleries`;
+#DROP TABLE IF EXISTS `#__rsg2_images`;
+#DROP TABLE IF EXISTS `#__rsg2_comments`;
+#DROP TABLE IF EXISTS `#__rsg2_acl`;
 
-#DROP TABLE IF EXISTS `#__rsgallery2_dummy_is_eaten`;
-#DROP TABLE IF EXISTS `#__rsgallery2_galleries`;
-#DROP TABLE IF EXISTS `#__rsgallery2_files`;
-#DROP TABLE IF EXISTS `#__rsgallery2_comments`;
-#DROP TABLE IF EXISTS `#__rsgallery2_config`;
-#DROP TABLE IF EXISTS `#__rsgallery2_acl`;
+#
+# uninstall sql for RSGallery2 J! Version 1.5/2.5/3.x
+#
 
+#DROP TABLE IF EXISTS `#__rsg2_galleries`;
+#DROP TABLE IF EXISTS `#__rsg2_files`;
+#DROP TABLE IF EXISTS `#__rsg2_comments`;
+#DROP TABLE IF EXISTS `#__rsg2_config`;
+#DROP TABLE IF EXISTS `#__rsg2_acl`;
 
