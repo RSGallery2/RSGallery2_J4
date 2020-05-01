6b77f96161df219262decea575aeabaa0f451279
Thomas.Finnern <github@tomfinnern.de>
2020-05-01 12:44:35 +0200

    Prepared standard backup calls

---
+++ .test/BackupRestore/BackupRsg2.J4x_Sim3x.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+REM Backups RSG2 database tables and RSG2 files
+
+CLS
+
+ECHO ------------------------------------------
+CALL BackupRsg2.para.bat "d:\xampp\htdocs" "joomla4x_sim3x"
+ECHO ------------------------------------------
+
+pause
--- .test/BackupRestore/BackupRsg2.set.01.bat
+++ .test/BackupRestore/BackupRsg2.set.01.bat
@@ -1,10 +1,17 @@
 @ECHO OFF
 REM Backups RSG2 database tables and RSG2 files
+REM Set 01: FithDell5570
 
 CLS
 
 ECHO ------------------------------------------
+ECHO Set 01: FithDell5570 (Main PC)
+
+ECHO ------------------------------------------
 CALL BackupRsg2.para.bat "d:\xampp\htdocs" "joomla4x"
+
+ECHO ------------------------------------------
+CALL BackupRsg2.para.bat "d:\xampp\htdocs" "joomla4x_Sim3x"
 
 ECHO ------------------------------------------
 CALL BackupRsg2.para.bat "d:\xampp\htdocs" "joomla3x"
@@ -14,8 +21,5 @@ CALL BackupRsg2.para.bat "d:\xampp\htdocs" "joomla3xNextRelease"
 
 ECHO ------------------------------------------
 CALL BackupRsg2.para.bat "d:\xampp\htdocs" "joomla3xRelease"
-
-ECHO ------------------------------------------
-CALL BackupRsg2.para.bat "d:\xampp\htdocs" "joomla4xOld"
 
 ECHO ------------------------------------------
