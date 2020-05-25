6c2bb4fa40357133e15ab8dc1ac93b66ebe51e31
ThomasFinnern <ThomasFinnernDell5540@users.noreply.github.com>
2020-05-25 09:02:37 +0200

    Moved to RSGallery2_J4_Dev

--- .build/clean4release.bat
+++
@@ -1,8 +0,0 @@
-REM Deletes files which are often copied 
-REM from web sources but not at the right place
-REM THEY are not in git but may be accidently 
-REM added to the release data 
-
-del ..\admin\install.rsgallery2.php
-del ..\admin\rsgallery2.xml 
-
