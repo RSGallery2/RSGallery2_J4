d36cafdad38c3525d014e14c83e77e2cbec9ac4e
ThomasFinnern <ThomasFinnernDell5540@users.noreply.github.com>
2020-12-04 16:42:30 +0100

    Prepared 4 build -> new artefacts

---
+++ modules/prepared/mod_rsg2_galleries/tmpl/default.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  mod_rsg2_images
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+\defined('_JEXEC') or die;
+
+//HTMLHelper::_('stylesheet', 'com_rsgallery2/maintenance.css', array('version' => 'auto', 'relative' => true));
+/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+$wa = $app->getDocument()->getWebAssetManager();
+$wa->registerAndUseStyle('mod_rsg2_images', 'mod_rsg2_images/image_grid.css');
+
+//echo '[PROJECT_NAME]' . $test . '<br />' . $url;
+echo '[PROJECT_NAME]'  . '<br />' . $folderUrl  . '<br />' . '<br /><hr> ';
+
+//foreach ($images as $image) {
+//
+//    echo $image . '<br />';
+//
+//}
+//
+
+//             <div class="grid-element"><img src="<?php echo $image; ? >"> </div>
+?>
+
+<div class="grid-container">
+        <?php foreach ($images as $image) : ?>
+
+            <figure class=”gallery__item gallery__item--1">
+                <img src="<?php echo $image; ?>" class="gallery__img" alt="Image 1">
+            </figure>
+
+        <?php endforeach; ?>
+    </div>
+</div>
