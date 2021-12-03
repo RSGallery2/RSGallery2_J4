4ec4fd09dfaebe1af28313726a9897f63faedcff
ThomasFinnern <ThomasFinnernDell5540@users.noreply.github.com>
2021-12-03 19:08:35 +0100

    Root legacy display improvments

--- components/com_rsgallery2/tmpl/rsg2_legacy/default.php
+++ components/com_rsgallery2/tmpl/rsg2_legacy/default.php
@@ -103,7 +103,6 @@ $displayData['pagination'] = $this->pagination;
          *         -> Rsg2_legacy is wrong
          *
         */
-
         </pre>
 
         <pre>
@@ -128,18 +127,250 @@ $displayData['pagination'] = $this->pagination;
 
         <?php
 		// RSGallery2_Project\Documentation\J!3x\ImagesUsedInDoc\site.start.rootgalleries.png
-	    echo '$layout_latest root ... galleries  ->render';
+	    echo 'ToDo: $layout_ root ... galleries  ->render';
 	    ?>
 
-        <?php
-		// RSGallery2_Project\Documentation\J!3x\ImagesUsedInDoc\site.start.randomLatestImages.png
-	    echo '$layout_random images ->render';
-	    ?>
+        <div class="rsg2-pagenav-limitbox">
+		<form action="/index.php/demo/demo-menu-root-galleries" method="post">
+			<select id="limit" name="limit" class="inputbox input-mini" size="1" onchange="this.form.submit()">
+				<option value="5" selected="selected">5</option>
+				<option value="10">10</option>
+				<option value="15">15</option>
+				<option value="20">20</option>
+				<option value="25">25</option>
+				<option value="30">30</option>
+				<option value="50">50</option>
+				<option value="100">100</option>
+				<option value="0">All</option>
+			</select>
+		</form>
+	</div>
 
-        <?php
-		// RSGallery2_Project\Documentation\J!3x\ImagesUsedInDoc\site.start.randomLatestImages.png
-	    echo '$layout_latest images ->render';
-	    ?>
+
+	<div class="rsg_galleryblock">
+		<div class="rsg2-galleryList-status"></div>
+		<div class="rsg2-galleryList-thumb">
+			<div class="img-shadow">
+				<a href="/index.php/demo/demo-menu-root-galleries/gallery/7">
+					<img class="rsg2-galleryList-thumb" src="http://rsgallery2.org/images/rsgallery/original/img_9114.jpg" alt="">
+				</a>
+			</div>
+		</div>
+		<div class="rsg2-galleryList-text">
+			NextGallery <span class="rsg2-galleryList-newImages">
+				<sup></sup>
+			</span>
+			<div class="rsg_gallery_details">
+				<div class="rsg2_details">
+					<a href="/index.php/demo/demo-menu-root-galleries/gallery/7/asSlideshow">Slideshow</a><br>
+						Owner: adminfinnern<br>
+						Size: 7 images<br>
+						Created: 16 August 2020<br>
+				</div>
+			</div>
+			<div class="rsg2-galleryList-description">
+			</div>
+		</div>
+		<div class="rsg_sub_url_single">
+		</div>
+	</div>
+	
+	
+	<div class="rsg_galleryblock">
+		<div class="rsg2-galleryList-status"></div>
+		<div class="rsg2-galleryList-thumb">
+			<div class="img-shadow">
+				<a href="/index.php/demo/demo-menu-root-galleries/gallery/5">
+					<img class="rsg2-galleryList-thumb" src="http://rsgallery2.org/images/rsgallery/thumb/2015-10-11_00002.jpg.jpg" alt="">
+				</a>
+			</div>
+		</div>
+		<div class="rsg2-galleryList-text">
+			Love Locks			<span class="rsg2-galleryList-newImages">
+				<sup></sup>
+			</span>
+			<div class="rsg_gallery_details">
+				<div class="rsg2_details">
+					<a href="/index.php/demo/demo-menu-root-galleries/gallery/5/asSlideshow">
+						Slideshow</a>
+					<br>
+					Owner: adminfinnern	<br>
+					Size: 44 images<br>
+					Created: 16 August 2020<br>
+				</div>
+			</div>
+			<div class="rsg2-galleryList-description">
+			</div>
+		</div>
+		<div class="rsg_sub_url_single">
+		</div>
+	</div>
+
+	<div class="rsg_galleryblock">
+		<div class="rsg2-galleryList-status"></div>
+		<div class="rsg2-galleryList-thumb">
+			<div class="img-shadow">
+				<a href="/index.php/demo/demo-menu-root-galleries/gallery/4">
+					<img class="rsg2-galleryList-thumb" src="http://rsgallery2.org/images/rsgallery/thumb/test_600x600-1.jpg.jpg" alt="">
+				</a>
+			</div>
+		</div>
+		<div class="rsg2-galleryList-text">
+			Third gallery			<span class="rsg2-galleryList-newImages">
+				<sup></sup>
+			</span>
+			<div class="rsg_gallery_details">
+				<div class="rsg2_details">
+					<a href="/index.php/demo/demo-menu-root-galleries/gallery/4/asSlideshow">
+							Slideshow</a>
+					<br>
+					Owner: adminfinnern						<br>
+					Size: 10 images						<br>
+					Created: 16 August 2020						<br>
+				</div>
+			</div>
+			<div class="rsg2-galleryList-description">			
+			</div>
+		</div>
+		<div class="rsg_sub_url_single">
+		</div>
+	</div>
+
+...
+	
+	<div class="pagination">
+		<br>
+		Results 1 - 5 of 5	
+	</div>
+	
+	<div class="rsg2-clr"></div>
+
+	<ul id="rsg2-galleryList">
+		<li class="rsg2-galleryList-item">
+			<table class="table_border" cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tbody>
+					<tr>
+						<td colspan="3">Random images</td>
+					</tr>
+					<tr>
+						<td colspan="3">&nbsp;</td>
+					</tr>
+					<tr>
+						<td align="center">
+							<div class="shadow-box">
+								<div class="img-shadow">
+									<a href="/index.php/demo/demo-menu-root-galleries/item/6/asInline">
+										<img src="http://rsgallery2.org/images/rsgallery/thumb/test_150x150.jpg.jpg" alt="test_150x150" width="80">
+									</a>
+								</div>
+								<div class="rsg2-clr">
+								</div>
+								<div class="rsg2_details">Uploaded:&nbsp;Sunday, 16 August 2020</div>
+							</div>
+						</td>
+						<td align="center">
+							<div class="shadow-box">
+								<div class="img-shadow">
+									<a href="/index.php/demo/demo-menu-root-galleries/item/17/asInline">
+										<img src="http://rsgallery2.org/images/rsgallery/original/154_5497.jpg" alt="154_5497" width="80">
+									</a>
+								</div>
+								<div class="rsg2-clr">
+								</div>
+								<div class="rsg2_details">Uploaded:&nbsp;Sunday, 16 August 2020</div>
+							</div>
+						</td>
+						<td align="center">
+							<div class="shadow-box">
+								<div class="img-shadow">
+									<a href="/index.php/demo/demo-menu-root-galleries/item/62/asInline">
+										<img src="http://rsgallery2.org/images/rsgallery/thumb/2015-10-11_00012-1.jpg.jpg" alt="2015-10-11_00012-1" width="80">
+									</a>
+								</div>
+								<div class="rsg2-clr">
+								</div>
+								<div class="rsg2_details">Uploaded:&nbsp;Sunday, 16 August 2020</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="3">&nbsp;</td>
+					</tr>
+				</tbody>
+			</table>
+		</li>
+	</ul>
+	
+
+
+	<ul id="rsg2-galleryList">
+		<li class="rsg2-galleryList-item">
+			<table class="table_border" cellspacing="0" cellpadding="0" border="0" width="100%">
+				<tbody>
+					<tr>
+						<td colspan="3">Latest images</td>
+					</tr>
+					<tr>
+						<td colspan="3">&nbsp;</td>
+					</tr>
+					<tr>
+						<td align="center">
+							<div class="shadow-box">
+								<div class="img-shadow">
+									<a href="/index.php/demo/demo-menu-root-galleries/item/88/asInline">
+										<img src="http://rsgallery2.org/images/rsgallery/original/dsc_5520.jpg" alt="dsc_5520" width="80">
+									</a>
+								</div>
+								<div class="rsg2-clr">
+								</div>
+								<div class="rsg2_details">Uploaded:&nbsp;Sunday, 16 August 2020</div>
+							</div>
+						</td>
+						<td align="center">
+							<div class="shadow-box">
+								<div class="img-shadow">
+									<a href="/index.php/demo/demo-menu-root-galleries/item/89/asInline">
+										<img src="http://rsgallery2.org/images/rsgallery/original/dsc_5526.jpg" alt="dsc_5526" width="80">
+									</a>
+								</div>
+								<div class="rsg2-clr">
+								</div>
+								<div class="rsg2_details">Uploaded:&nbsp;Sunday, 16 August 2020</div>
+							</div>
+						</td>
+						<td align="center">
+							<div class="shadow-box">
+								<div class="img-shadow">
+									<a href="/index.php/demo/demo-menu-root-galleries/item/90/asInline">
+										<img src="http://rsgallery2.org/images/rsgallery/original/dsc_5527.jpg" alt="dsc_5527" width="80">
+									</a>
+								</div>
+								<div class="rsg2-clr"></div>
+								<div class="rsg2_details">Uploaded:&nbsp;Sunday, 16 August 2020</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td colspan="3">&nbsp;</td>
+					</tr>
+				</tbody>
+			</table>
+		</li>
+	</ul>
+
+
+
+        <?php if ((int) $params->get('displayLatest', 0) === 0) : ?>
+            <?php echo $this->loadTemplate('latest_images'); ?>
+        <?php endif; ?>
+
+        <?php if ((int) $params->get('displayRandom', 0) === 0) : ?>
+            <?php echo $this->loadTemplate('random_images'); ?>
+        <?php endif; ?>
+
+
+
+
 
 
     </form>
---
+++ components/com_rsgallery2/tmpl/rsg2_legacy/default_random_images.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_rsgallery2
+ *
+ * @copyright   (C) 2005 - 2021 RSGallery2 Team
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Language\Text;
+
+
+// RSGallery2_Project\Documentation\J!3x\ImagesUsedInDoc\site.start.randomLatestImages.png
+echo '$layout_random images ->render';
+
