1ebd78ad7095965ad675b314620460cfed512b96
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2021-12-04 20:02:53 +0100

    Root legacy display improvments

--- components/com_rsgallery2/tmpl/rsg2_legacy/default_latest_images.php
+++ components/com_rsgallery2/tmpl/rsg2_legacy/default_latest_images.php
@@ -13,8 +13,64 @@ use Joomla\CMS\Language\Text;
 
 
 // RSGallery2_Project\Documentation\J!3x\ImagesUsedInDoc\site.start.randomLatestImages.png
-echo '$layout_latest images ->render';
 
+
+?>
+
+<ul id="rsg2-galleryList">
+	<li class="rsg2-galleryList-item">
+		<table class="table_border" cellspacing="0" cellpadding="0" border="0" width="100%">
+			<tbody>
+			<tr>
+				<td colspan="3">Latest images</td>
+			</tr>
+			<tr>
+				<td colspan="3">&nbsp;</td>
+			</tr>
+			<tr>
+				<td align="center">
+					<div class="shadow-box">
+						<div class="img-shadow">
+							<a href="/index.php/demo/demo-menu-root-galleries/item/88/asInline">
+								<img src="http://rsgallery2.org/images/rsgallery/original/dsc_5520.jpg" alt="dsc_5520" width="80">
+							</a>
+						</div>
+						<div class="rsg2-clr">
+						</div>
+						<div class="rsg2_details">Uploaded:&nbsp;Sunday, 16 August 2020</div>
+					</div>
+				</td>
+				<td align="center">
+					<div class="shadow-box">
+						<div class="img-shadow">
+							<a href="/index.php/demo/demo-menu-root-galleries/item/89/asInline">
+								<img src="http://rsgallery2.org/images/rsgallery/original/dsc_5526.jpg" alt="dsc_5526" width="80">
+							</a>
+						</div>
+						<div class="rsg2-clr">
+						</div>
+						<div class="rsg2_details">Uploaded:&nbsp;Sunday, 16 August 2020</div>
+					</div>
+				</td>
+				<td align="center">
+					<div class="shadow-box">
+						<div class="img-shadow">
+							<a href="/index.php/demo/demo-menu-root-galleries/item/90/asInline">
+								<img src="http://rsgallery2.org/images/rsgallery/original/dsc_5527.jpg" alt="dsc_5527" width="80">
+							</a>
+						</div>
+						<div class="rsg2-clr"></div>
+						<div class="rsg2_details">Uploaded:&nbsp;Sunday, 16 August 2020</div>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="3">&nbsp;</td>
+			</tr>
+			</tbody>
+		</table>
+	</li>
+</ul>
 
 
 
