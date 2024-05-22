6d08878537003bcdb45d62c076d07202d70c8bfd
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2024-05-22 16:49:03 +0200

    base data highway for plugin rsg2_galleries

--- plugins/content/rsg2_galleries/src/Extension/Rsg2_galleries.php
+++ plugins/content/rsg2_galleries/src/Extension/Rsg2_galleries.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Rsgallery2\Plugin\Content\Rsg2_gallery\Extension;
+namespace Rsgallery2\Plugin\Content\Rsg2_galleries\Extension;
 
 /**
  * @package     Joomla.Plugin
@@ -19,7 +19,7 @@ use Joomla\Registry\Registry;
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
 
-class Rsg2_gallery extends CMSPlugin implements SubscriberInterface
+class Rsg2_galleries extends CMSPlugin implements SubscriberInterface
 {
 	// only for lang strings shown on execution of plugin
 	protected $autoloadLanguage = true;
@@ -34,11 +34,11 @@ class Rsg2_gallery extends CMSPlugin implements SubscriberInterface
 	public static function getSubscribedEvents () : array
 	{
 		return [
-			'onContentPrepare' => 'getRsg2_galleryDisplay'
+			'onContentPrepare' => 'getRsg2_galleriesDisplay'
 		];
 	}
 
-	public function getRsg2_galleryDisplay (Event $event)
+	public function getRsg2_galleriesDisplay (Event $event)
 	{
 		$context = '';
 		$article = '';
@@ -55,26 +55,28 @@ class Rsg2_gallery extends CMSPlugin implements SubscriberInterface
 				$params = $event->getArgument('params'); // spelling ?
 			}
 
-			if (strpos ($context , 'com_content.article') === false) {
+			if (   (strpos ($context , 'com_content.article') === false)
+				&& (strpos ($context , 'com_content.category') === false)
+			) {
 				return false;
 			}
 
 			//--- replacement may exist --------------------------------------------------
 
-			if (str_contains($article->text, '{rsg2_gallery'))
+			if (str_contains($article->text, '{rsg2_galleries'))
 			{
 				$lastUserTestIdx = 0;
 
 				//--- collect all appearances ---------------------------------------
 
 				// Expression to search for.
-				$pattern = "/{rsg2_gallery:(.*?)}/i";
+				$pattern = "/{rsg2_galleries:(.*?)}/i";
 
 				preg_match_all($pattern, $article->text, $matches, PREG_SET_ORDER);
 
 //				// debug: there should be matches as text is searched
 //				if(empty ($matches)) {
-//					echo "<br><br>!!! article has no rsg2_gallery !!!<br>";
+//					echo "<br><br>!!! article has no rsg2_galleries !!!<br>";
 //					return null;
 //				}
 
@@ -98,8 +100,8 @@ class Rsg2_gallery extends CMSPlugin implements SubscriberInterface
 
 
 						$insertHtml = '';
-						// $insertHtml = Rsg2_galleryHelper::galleryImagesHtml();
-						$insertHtml = '<h4>--- Rsg2_gallery replacement ---</h4>' . $insertHtml;
+						// $insertHtml = Rsg2_galleriesHelper::galleryImagesHtml();
+						$insertHtml = '<h4>--- Rsg2_galleries replacement ---</h4>' . $insertHtml;
 						
 						// previous occurrences are replaced so the search will
 						// find the actual occurrence
@@ -120,7 +122,7 @@ class Rsg2_gallery extends CMSPlugin implements SubscriberInterface
 			// $event->stopPropagation();
 
 		} catch (Exception $e) {
-			$msg = Text::_('PLG_CONTENT_RSG2_GALLERY') . ' getRsg2_galleryDisplay: '. ' Error (01): ' . $e->getMessage();
+			$msg = Text::_('PLG_CONTENT_RSG2_GALLERIES') . ' getRsg2_galleriesDisplay: '. ' Error (01): ' . $e->getMessage();
 			$app = Factory::getApplication();
 			$app->enqueueMessage($msg, 'error');
 			return false;
@@ -162,7 +164,7 @@ class Rsg2_gallery extends CMSPlugin implements SubscriberInterface
 
 			}
 		} catch (Exception $e) {
-			$msg = Text::_('PLG_CONTENT_RSG2_GALLERY' . 'extractUserParams: "') . $usrString . '" Error (01): ' . $e->getMessage();
+			$msg = Text::_('PLG_CONTENT_RSG2_GALLERIES' . 'extractUserParams: "') . $usrString . '" Error (01): ' . $e->getMessage();
 			$app = Factory::getApplication();
 			$app->enqueueMessage($msg, 'error');
 			return false;
---
+++ plugins/content/rsg2_random_images/services/provider.php
@@ -0,0 +1,42 @@
+<?php 
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\PluginInterface;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
+use Joomla\Database\DatabaseInterface;
+use Joomla\DI\Container;
+use Joomla\DI\ServiceProviderInterface;
+use Joomla\Event\DispatcherInterface;
+
+use Rsgallery2\Plugin\Content\Rsg2_gallery\Extension\Rsg2_gallery;
+
+
+return new class () implements ServiceProviderInterface {
+    /**
+     * Registers the service provider with a DI container.
+     *
+     * @param   Container  $container  The DI container.
+     *
+     * @return  void
+     *
+     * @since   4.3.0
+     */
+    public function register(Container $container)
+    {
+        $container->set(
+            PluginInterface::class,
+            function (Container $container) {
+				$plgHelper =(array)PluginHelper::getPlugin('content', 'rsg2_gallery');
+                $dispatcher = $container->get(DispatcherInterface::class);
+
+                $plugin     = new Rsg2_gallery($dispatcher, $plgHelper);
+                $plugin->setApplication(Factory::getApplication());
+                //$plugin->setDatabase($container->get(DatabaseInterface::class));
+				//$plugin->setMyCustomService($container->get(MyCustomService::class));
+
+                return $plugin;
+            }
+        );
+    }
+};
