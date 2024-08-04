9ff5209bd0e00776f3ebed740829c4c0db92d88b
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2024-08-04 21:53:16 +0200

    tests upgrade  *.sql file

--- administrator/components/com_rsgallery2/sql/updates/mysql/5.0.12.4.sql
+++ administrator/components/com_rsgallery2/sql/updates/mysql/5.0.12.4.sql
@@ -1,26 +1,16 @@
-#
-# upgrade db to RSGallery2 J4x++ 5.0.12
-#
+# IF EXISTS(SELECT table_name
+#     FROM INFORMATION_SCHEMA.TABLES
+#     WHERE table_schema = 'joomla4x'
+#              AND table_name LIKE gsqft_rsgallery2_galleries)
+# THEN
+#     ALTER TABLE gsqft_rsgallery2_galleries MODIFY checked_out_time datetime NOT NULL;
+# ENDIF;
 
-# datetime: DEFAULT '0000-00-00 00:00:00' brings error in J!4 mmysql
-
-ALTER TABLE `#__rsgallery2_galleries` MODIFY `checked_out_time` datetime NOT NULL;
-ALTER TABLE `#__rsgallery2_galleries` MODIFY `date` datetime NOT NULL;
-
-ALTER TABLE `#__rsgallery2_files` MODIFY `checked_out_time` datetime NOT NULL;
-ALTER TABLE `#__rsgallery2_files` MODIFY `date` datetime NOT NULL;
-
-ALTER TABLE `#__rsgallery2_comments` MODIFY `checked_out_time` datetime NOT NULL;
-ALTER TABLE `#__rsgallery2_comments` MODIFY `datetime` datetime NOT NULL;
-
-
-UPDATE `#__rsgallery2_galleries` SET `checked_out_time` = '1980-01-01 00:00:00' WHERE `checked_out_time` = '0000-00-00 00:00:00';
-UPDATE `#__rsgallery2_galleries` SET `date` = '1980-01-01 00:00:00' WHERE `date` = '0000-00-00 00:00:00';
-
-UPDATE `#__rsgallery2_files` SET `checked_out_time` = '1980-01-01 00:00:00' WHERE `checked_out_time` = '0000-00-00 00:00:00';
-UPDATE `#__rsgallery2_files` SET `date` = '1980-01-01 00:00:00' WHERE `date` = '0000-00-00 00:00:00';
-
-UPDATE `#__rsgallery2_comments` SET `checked_out_time` = '1980-01-01 00:00:00' WHERE `checked_out_time` = '0000-00-00 00:00:00';
-UPDATE `#__rsgallery2_comments` SET `datetime` = '1980-01-01 00:00:00' WHERE `datetime` = '0000-00-00 00:00:00';
-
+IF EXISTS(SELECT table_name
+    FROM INFORMATION_SCHEMA.TABLES
+    WHERE table_schema = 'joomla4x'
+             AND table_name = 'gsqft_rsgallery2_galleries')
+BEGIN
+    ALTER TABLE 'gsqft_rsgallery2_galleries' MODIFY checked_out_time datetime NOT NULL;
+END;
 
