4ada5594bcadd3adccf5e4374859149d9175723b
ThomasFinnern <ThomasFinnernDell5540@users.noreply.github.com>
2024-09-19 13:47:18 +0200

    upgrade file header

--- administrator/components/com_rsgallery2/src/Model/GalleryTreeModel.php
+++ administrator/components/com_rsgallery2/src/Model/GalleryTreeModel.php
@@ -2,10 +2,8 @@
 /**
  * @package         RSGallery2
  * @subpackage      com_rsgallery2
- *
  * @author          RSGallery2 Team <team2@rsgallery2.org>
  * @copyright  (c)  2020-2024 RSGallery2 Team
- * @license         GNU General Public License version 2 or later
  * @license         GNU General Public License version 2 or later
  */
 
