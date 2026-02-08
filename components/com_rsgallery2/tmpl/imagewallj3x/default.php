70c8602b08405b95367b8e67b27a80ac62d9c86d
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2026-02-08 21:11:23 +0100

    Checked column and row sizes of all layouts (+/-13px ...)

--- components/com_rsgallery2/tmpl/imagewallj3x/default.php
+++ components/com_rsgallery2/tmpl/imagewallj3x/default.php
@@ -21,16 +21,12 @@ use Joomla\CMS\Router\Route;
 
 $this->document->getWebAssetManager()->usePreset('com_rsgallery2.site.imageWallJ3x');
 
-//$layoutName = 'yyy.default';
-//$layout = new FileLayout($layoutName);
+//--- retrieve img wall layout --------------------------------
 
-//--- 04 ------------------------------------------
-// $layoutName = 'ImageWall.overlapping_flex';
-//--- 02 ------------------------------------------
-// $layoutName = 'ImageWall.overlapping_black';
-//--- 03 ------------------------------------------
-$layoutName = 'ImageWall.rectangle_plain';
+$params    = $this->params;
 
+$layoutSelection = $params->get('layout_img_wall');
+$layoutName = 'Imagewall.' . $layoutSelection;
 
 $displayData['images'] = $this->items;
 
@@ -46,6 +42,15 @@ if (!empty($this->isDebugSite)) {
 echo Route::_('index.php?option=com_rsgallery2&view=rootgalleriesj3x'); ?>"
       method="post" class="form-validate form-horizontal well">
 
+    <?php if (!empty($this->isDebugSite)) : ?>
+        <h1><?php echo text::_('RSGallery2 imagewallj3x'); ?> view </h1>
+        <hr>
+    <?php endif; ?>
+
+
+
+
+    <?php //--- image wall ------------------------------------------ ?>
     <?php
     $layout = new FileLayout($layoutName);
     echo $layout->render($displayData);
@@ -53,147 +58,4 @@ echo Route::_('index.php?option=com_rsgallery2&view=rootgalleriesj3x'); ?>"
 
 
 
-    <?php //--- 03 ------------------------------------------
-    ?>
-
-<!--    <div class="image-wall">-->
-<!---->
-<!--        --><?php //foreach ($this->items as $idx => $gallery) : ?>
-<!---->
-<!--            <img class="overlapping-img"-->
-<!--                 src="--><?php //echo $gallery->UrlDisplayFile ?><!--" alt="--><?php //echo $gallery->name; ?><!--">-->
-<!---->
-<!--        --><?php //endforeach; ?>
-<!--    </div>-->
-<!---->
-    <?php //--- 02 ------------------------------------------
-    ?>
-    <!---->
-    <!--        <div class="imgWall">-->
-    <!--    -->
-    <!--        --><?php //foreach ($this->items as $idx => $gallery) :
-    ?>
-    <!---->
-    <!--            <img class="overlapping-img"-->
-    <!--                 src="--><?php //echo $gallery->UrlDisplayFile
-    ?><!--" alt="--><?php //echo $gallery->name;
-    ?><!--">-->
-    <!---->
-    <!--        --><?php //endforeach;
-    ?>
-    <!--        </div>-->
-
-    <?php //--- 0x ------------------------------------------
-    ?>
-
-    <!--        <table class="blend-table">-->
-    <!---->
-    <!--            --><?php //foreach ($this->items as $idx => $gallery) :
-    ?>
-    <!---->
-    <!--                --><?php
-    //                $colIdx = $idx % $maxCols;
-    //                $rowIdx = intdiv($idx, $maxCols);
-    //
-    ?>
-    <!---->
-    <!--                --><?php //if ($colIdx == 0) :
-    ?>
-    <!--                    <tr>-->
-    <!--                --><?php //endif
-    ?>
-    <!---->
-    <!--                <td>-->
-    <!--                    <img class="overlapping-img"-->
-    <!--                         src="--><?php //echo $gallery->UrlDisplayFile
-    ?><!--" alt="--><?php //echo $gallery->name;
-    ?><!--">-->
-    <!--                </td>-->
-    <!---->
-    <!--                --><?php //if ($colIdx == $maxCols - 1) :
-    ?>
-    <!--                    </tr>-->
-    <!--                --><?php //endif
-    ?>
-    <!---->
-    <!--            --><?php //endforeach;
-    ?>
-    <!---->
-    <!--        </table>-->
-    <!--    </div>-->
-
-    <?php //--- 01 ------------------------------------------
-    ?>
-
-    <!--        <table class="table-imgWall">-->
-    <!--            <tbody>-->
-    <!---->
-    <!--            --><?php //foreach ($this->items as $idx => $gallery) :
-    ?>
-    <!---->
-    <!--                --><?php ////--- image wall classes of element -------------------------
-    ?>
-    <!---->
-    <!--                --><?php
-    //                $colIdx = $idx % $maxCols;
-    //                $rowIdx = intdiv($idx, $maxCols);
-    //
-    ?>
-    <!---->
-    <!--                --><?php
-    //
-    //                // ToDo: make function above / below
-    //                //--- image wall classes of element -------------------------
-    //
-    //                // left upper edge
-    //                if ($colIdx == 0 && $rowIdx == 0) {
-    //                    $wallClasses = 'left-col top-row';
-    //                } else {
-    //                    // left column
-    //                    if ($colIdx == 0) {
-    //                        $wallClasses = 'left-col';
-    //                    } else {
-    //                        // upper row
-    //                        if ($rowIdx == 0) {
-    //                            $wallClasses = 'top-row';
-    //                        } else {
-    //                            $wallClasses = 'inner-cell';
-    //                        }
-    //                    }
-    //                }
-    //
-    //
-    ?>
-    <!---->
-    <!---->
-    <!--                --><?php ////--- Table parts -------------------------
-    ?>
-    <!---->
-    <!--                --><?php //if ($colIdx == 0) :
-    ?>
-    <!--                    <tr>-->
-    <!--                --><?php //endif
-    ?>
-    <!---->
-    <!--                <td>-->
-    <!--                    <img class="--><?php //echo $wallClasses
-    ?><!--"-->
-    <!--                         src="--><?php //echo $gallery->UrlDisplayFile
-    ?><!--" alt="--><?php //echo $gallery->name;
-    ?><!--">-->
-    <!--                </td>-->
-    <!---->
-    <!--                --><?php //if ($colIdx == $maxCols - 1) :
-    ?>
-    <!--                    </tr>-->
-    <!--                --><?php //endif
-    ?>
-    <!---->
-    <!--            --><?php //endforeach;
-    ?>
-    <!---->
-    <!--            </tbody>-->
-    <!--        </table>-->
-
-    <!--    </div>-->
 </form>
