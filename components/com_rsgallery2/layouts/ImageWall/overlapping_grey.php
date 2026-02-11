6fb5a5845c2fe7710b1a8bfb1a67d169c9f75bf2
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2026-02-11 12:24:47 +0100

    image wall: patched black areas with white images

--- components/com_rsgallery2/layouts/ImageWall/overlapping_grey.php
+++ components/com_rsgallery2/layouts/ImageWall/overlapping_grey.php
@@ -13,12 +13,15 @@ namespace Rsgallery2\Component\Rsgallery2\Site\Layouts\ImageWall;
 // phpcs:disable PSR1.Files.SideEffects
 use Joomla\CMS\Uri\Uri;
 
+// phpcs:enable PSR1.Files.SideEffects
+
 \defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
 
-/*---------------------------------------------------
-gallery thumbs display by rows like in J3x
----------------------------------------------------*/
+/*--------------------------------------------------------------
+Gallery of images with overlapping, blended bright borders
+Background of image area is grey
+--------------------------------------------------------------*/
 
 extract($displayData);
 
@@ -44,18 +47,28 @@ if (!isset($images)) {
     $images = [];
 }
 
-$noImageUrl = URI::root() . '/media/com_rsgallery2/images/GalleryZeroImages.svg';
-$missingUrl = URI::root() . '/media/com_rsgallery2/images/ImageQuestionmark.svg';
-
-//--- assign dummy images if not found -----------------------------------
+$noImageUrl     = URI::root() . '/media/com_rsgallery2/images/GalleryZeroImages.svg';
+$missingUrl     = URI::root() . '/media/com_rsgallery2/images/ImageQuestionmark.svg';
+$plane_whiteUrl = URI::root() . '/media/com_rsgallery2/images/pure_white.jpg';
 
 if (!empty($images)) {
+    //--- assign dummy white images  -----------------------------------
+
+    // Images left out in area.
+    // CSS will create 2,3 or 4 columns. Missing image would appear as black areas
+    // White images must be added to cover the black background.
+    $imgCount    = count($images);
+    $imgReplaces = 12 - $imgCount % 12;
+
+    //--- assign url to images  -----------------------------------
+
+    // Display all images.
     foreach ($images as $idx => $image) {
         // show dummy thumb on galleries with no images
         if (!empty($image->isHasNoImages)) {
             $image->UrlOriginalFile = $noImageUrl;
             $image->UrlDisplayFiles = $noImageUrl;
-            $image->UrlThumbFile = $noImageUrl;
+            $image->UrlThumbFile    = $noImageUrl;
         }
 
 //    else {
@@ -77,21 +90,30 @@ if (!empty($images)) {
     }
 }
 
-$imgCount = count($images);
-
+// <span class="image-wall-img-background">
 ?>
 
-<?php //--- 04 ------------------------------------------
-?>
+<?php if (!empty($images)) : ?>
 
-<div class="image-wall-overlapping-flex">
+    <div class="image-wall-overlapping-grey">
 
-    <?php foreach ($images as $idx => $image) : ?>
+        <?php foreach ($images as $idx => $image) : ?>
 
-        <img class="overlapping-img"
-             src="<?php echo $image->UrlDisplayFile ?>" alt="<?php echo $image->name; ?>">
+            <img class="overlapping-img"
+                 src="<?php echo $image->UrlDisplayFile ?>" alt="<?php echo $image->name; ?>"
+            >
 
-    <?php endforeach; ?>
-</div>
+        <?php endforeach; ?>
+
+        <?php foreach (range(1, $imgReplaces) as $i) : ?>
+
+            <img class="overlapping-img"
+                 src="<?php echo $plane_whiteUrl ?>" alt="<?php echo "plain white image added"; ?>"
+            >
+
+        <?php endforeach; ?>
+    </div>
+
+<?php endif; ?>
 
 
