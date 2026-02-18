f8bac6028fc033569cb6a0cf4f4b4ca3fae2b68c
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2026-02-18 09:50:10 +0100

    Empty api upload controller file

---
+++ administrator/components/com_rsgallery2/src/Controller/UploadApiController.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @package        RSGallery2
+ * @subpackage     com_rsgallery2
+ * @author         RSGallery2 Team <team2@rsgallery2.org>
+ * @copyright  (c) 2016-2025 RSGallery2 Team
+ * @license        GNU General Public License version 2 or later
+ */
+
+namespace Rsgallery2\Component\Rsgallery2\Administrator\Controller;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+
+
+
+
+use function dirname;
+
+
+
+class UploadApiController extends BaseController
+{
+
+
+
+
+
+
+
+
+
+}
