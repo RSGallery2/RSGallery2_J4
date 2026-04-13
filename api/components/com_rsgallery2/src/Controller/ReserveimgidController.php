6e639a175f39a4f4b34b968d283cb6599d0a4515
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2026-04-13 17:26:36 +0200

    New ReserveimgidController db_reserve_image_id uses pparent:add

---
+++ api/components/com_rsgallery2/src/Controller/ReserveimgidController.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * @package        RSGallery2
+ * @subpackage     com_rsgallery2
+ * @author         RSGallery2 Team <team2@rsgallery2.org>
+ * @copyright  (c) 2005-2026 RSGallery2 Team
+ * @license        GNU General Public License version 2 or later
+ */
+
+namespace Rsgallery2\Component\Rsgallery2\Api\Controller;
+
+use Joomla\CMS\Filter\InputFilter;
+use Joomla\CMS\Helper\TagsHelper;
+use Joomla\CMS\MVC\Controller\ApiController;
+use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * The images controller
+ *
+ * @since  4.0.0
+ */
+class ReserveimgidController extends ImagesController // ApiController
+{
+    /**
+     * The content type of the item.
+     *
+     * @var    string
+     * @since  4.0.0
+     */
+    protected $contentType = 'images';
+
+    /**
+     * The default view for the display method.
+     *
+     * @var    string
+     * @since  3.0
+     */
+    protected $default_view = 'images';
+
+//    /**
+//     * Article list view amended to add filtering of data
+//     *
+//     * @return  static  A BaseController object to support chaining.
+//     *
+//     * @since   4.0.0
+//     */
+//    public function displayList()
+//    {
+////        $model = $this->getModel($this->contentType);
+////        $items = $model->getItems();
+////
+////        // Process and return the items
+////        return $items;
+//
+//        $apiFilterInfo = $this->input->get('filter', [], 'array');
+//        $filter        = InputFilter::getInstance();
+//
+//        if (\array_key_exists('created_by', $apiFilterInfo)) {
+//            $this->modelState->set('filter.created_by', $filter->clean($apiFilterInfo['created_by'], 'INT'));
+//        }
+//
+//        if (\array_key_exists('id', $apiFilterInfo)) {
+//            $this->modelState->set('filter.id', $filter->clean($apiFilterInfo['id'], 'INT'));
+//        }
+//
+//        if (\array_key_exists('title', $apiFilterInfo)) {
+//            $this->modelState->set('filter.title', $filter->clean($apiFilterInfo['title'], 'STRING'));
+//        }
+//
+//        if (\array_key_exists('alias', $apiFilterInfo)) {
+//            $this->modelState->set('filter.alias', $filter->clean($apiFilterInfo['alias'], 'STRING'));
+//        }
+//
+//        if (\array_key_exists('name', $apiFilterInfo)) {
+//            $this->modelState->set('filter.name', $filter->clean($apiFilterInfo['name'], 'INT'));
+//        }
+//
+//        if (\array_key_exists('notes', $apiFilterInfo)) {
+//            $this->modelState->set('filter.notes', $filter->clean($apiFilterInfo['notes'], 'STRING'));
+//        }
+//
+//        if (\array_key_exists('root_path', $apiFilterInfo)) {
+//            $this->modelState->set('filter.root_path', $filter->clean($apiFilterInfo['root_path'], 'STRING'));
+//        }
+//
+//      $apiListInfo = $this->input->get('list', [], 'array');
+//
+//      if (\array_key_exists('prjType', $apiFilterInfo)) {
+//            $this->modelState->set('list.prjType', $filter->clean($apiListInfo['prjType'], 'INT'));
+//        }
+//
+//      if (\array_key_exists('direction', $apiListInfo)) {
+//            $this->modelState->set('list.direction', $filter->clean($apiListInfo['direction'], 'STRING'));
+//        }
+//
+//        return parent::displayList();
+//    }
+
+//    public function create()
+//    {
+//        $data = $this->input->json->get('data', [], 'array');
+//
+//        $model = $this->getModel($this->contentType);
+//        $resourceId = $model->save($data);
+//
+//        if ($resourceId) {
+//            return $this->displayItem($resourceId);
+//        }
+//
+//        throw new \Exception('Failed to create resource');
+//    }
+
+
+    public function db_reserve_image_id (){
+
+        parent::add();
+    }
+
+    // Implement other methods like read, update, delete as needed
+}
