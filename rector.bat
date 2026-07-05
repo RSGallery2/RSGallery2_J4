a516d2c8060b4bc95ac7c19bac8b4ad0b7a93347
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2026-07-05 17:45:52 +0200

    Fix standard call

--- rector.bat
+++ rector.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-REM run actual rules of rector.php
+REM run with actual rules of rector.php
 
-ECHO vendor\bin\rector --dry-run
-vendor\bin\rector --dry-run
+ECHO vendor\bin\rector
+vendor\bin\rector
--- rector_dry.bat
+++ rector_dry.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-REM run actual rules of rector.php
+REM dry run with actual rules of rector.php
 
 ECHO vendor\bin\rector --dry-run
 vendor\bin\rector --dry-run
